c2944583f0d39159b92a0f5fdeb1c77c06767017
gzsomm <gzsomm@gmail.com>
2025-02-22 03:16:47 +0000

    Avance 1

--- src/app.py
+++ src/app.py
@@ -2,3 +2,7 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+
+df = pd.read_csv ('https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv')
+print (df.head())
+
